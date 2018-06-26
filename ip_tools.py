b2e6c305cd50d7907ea1dcb2a9c2628d09692fef
Dylan@ArchLutece <dylan@dylancromer.com>
2018-06-26 14:28:57 -0400

    Added pastebin and IP modules

---
+++ ip_tools.py
@@ -0,0 +1,20 @@
+import socket
+from requests import get
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    local_ip = s.getsockname()[0]
+    s.close()
+
+    return local_ip
+
+def get_public_ip():
+    ip = get('https://api.ipify.org').text
+    return ip
+
+if __name__ == '__main__':
+    local = get_local_ip()
+    print(local)
+    public = get_public_ip()
+    print(public)
