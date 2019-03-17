42ab3bdbcfef771a87685b4c0e9c77545c16010a
Dylan Cromer <dylanmattc@gmail.com>
2019-03-16 20:21:22 -0400

    deleted bad test

--- specs/flaregun_daemon_spec.py
+++ specs/flaregun_daemon_spec.py
@@ -11,18 +11,10 @@ def test_daemon_starts_flare():
 
     fd = FlareDaemon(failing_secret, dest_ip)
     fd.launch()
-    assert fd.flare.watching
+    assert fd._panicked
 
 """
     - when the flare gets signalled and fires, it reloads with a new flare and the next secret in the rotation
-"""
-def test_can_handle_reset():
-    secrets = ["Twitter", "otters love to eat soap, but it isn't good for them"]
-    dest_ip = "127.0.0.1"
-
-    fd = FlareDaemon(secrets, dest_ip)
-    fd.launch()
-"""
     - it remembers which secrets are "used up"
     - if it gets low on secrets, it says so
 """
